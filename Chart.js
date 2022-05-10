66b7051dad45813827f21430ea7b4e28540b287d
Peter Muessig <peter.muessig@sap.com>
2022-05-10 21:37:09 +0200

    build: added some more project config

--- Chart.js
+++ Chart.js
@@ -1,71 +1,71 @@
-sap.ui.define(["sap/ui/core/Control", "chart.js/auto", "sap/ui/dom/includeStylesheet"], function(Control, Chart, includeStylesheet) {
+sap.ui.define(["sap/ui/core/Control", "chart.js/auto", "sap/ui/dom/includeStylesheet"], function (Control, Chart, includeStylesheet) {
+	return Control.extend("ui5-cc-chart.Chart", {
+		metadata: {
+			properties: {
+				type: {
+					type: "string",
+					defaultValue: "line",
+				},
+				title: "string",
+				color: "sap.ui.core.CSSColor",
+			},
+			aggregations: {
+				records: {
+					type: "ui5-cc-chart.ChartRecord",
+				},
+			},
+			defaultAggregation: "records",
+		},
+		renderer: {
+			apiVersion: 2,
+			render: function (rm, chart) {
+				rm.openStart("div", chart);
+				rm.style("color", chart.getColor());
+				rm.class("chart");
+				rm.openEnd();
 
-    return Control.extend("ui5-cc-chart.Chart", {
-        metadata: {
-            properties: {
-                "type": {
-                    type: "string",
-                    defaultValue: "line"
-                },
-                "title": "string",
-                "color": "sap.ui.core.CSSColor"
-            },
-            aggregations: {
-                "records": {
-                    type: "ui5-cc-chart.ChartRecord"
-                }
-            },
-            defaultAggregation: "records"
-        },
-        renderer: {
-            apiVersion: 2,
-            render: function(rm, chart) {
-                rm.openStart("div", chart);
-                rm.style("color", chart.getColor());
-                rm.class("chart");
-                rm.openEnd();
+				rm.openStart("canvas", chart.getId() + "-canvas");
+				rm.openEnd();
+				rm.close("canvas");
 
-                rm.openStart("canvas", chart.getId() + "-canvas");
-                rm.openEnd();
-                rm.close("canvas");
-
-                rm.close("div");
-            }
-        },
-        _getChartData: function() {
-            const aRecords = this.getRecords();
-            return {
-                labels: aRecords.map(record => {
-                    return record.getLabel();
-                }),
-                datasets: [{
-                    label: this.getTitle(),
-                    backgroundColor: this.getColor(),
-                    borderColor: this.getColor(),
-                    data: aRecords.map(record => {
-                        return record.getValue();
-                    })
-                }]
-            };
-        },
-        init: function() {
-            includeStylesheet(sap.ui.require.toUrl("ui5-cc-chart/Chart.css"));
-        },
-        onAfterRendering: function() {
-            if (!this._chart) {
-                this._chart = new Chart(this.getDomRef("canvas"), {
-                    type: this.getType(),
-                    data: this._getChartData(),
-                    options: {
-                        responsive: true,
-                        animation: false
-                    }
-                });
-            } else {
-                this._chart.data = this._getChartData();
-                this._chart.update();
-            }
-        }
-    });
-
+				rm.close("div");
+			},
+		},
+		_getChartData: function () {
+			const aRecords = this.getRecords();
+			return {
+				labels: aRecords.map((record) => {
+					return record.getLabel();
+				}),
+				datasets: [
+					{
+						label: this.getTitle(),
+						backgroundColor: this.getColor(),
+						borderColor: this.getColor(),
+						data: aRecords.map((record) => {
+							return record.getValue();
+						}),
+					},
+				],
+			};
+		},
+		init: function () {
+			includeStylesheet(sap.ui.require.toUrl("ui5-cc-chart/Chart.css"));
+		},
+		onAfterRendering: function () {
+			if (!this._chart) {
+				this._chart = new Chart(this.getDomRef("canvas"), {
+					type: this.getType(),
+					data: this._getChartData(),
+					options: {
+						responsive: true,
+						animation: false,
+					},
+				});
+			} else {
+				this._chart.data = this._getChartData();
+				this._chart.update();
+			}
+		},
+	});
 });
--- ChartRecord.js
+++ ChartRecord.js
@@ -1,12 +1,10 @@
-sap.ui.define(["sap/ui/core/Element"], function(Element) {
-
-    return Element.extend("ui5-cc-chart.ChartRecord", {
-        metadata: {
-            properties: {
-                label: "string",
-                value: "float"
-            }
-        }
-    });
-
+sap.ui.define(["sap/ui/core/Element"], function (Element) {
+	return Element.extend("ui5-cc-chart.ChartRecord", {
+		metadata: {
+			properties: {
+				label: "string",
+				value: "float",
+			},
+		},
+	});
 });
