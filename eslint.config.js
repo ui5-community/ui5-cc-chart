2ded6e409873041bc1f9532b63a26e40c0e627d2
Peter Muessig <peter.muessig@sap.com>
2025-12-29 13:27:25 +0100

    build: update publish process

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+const globals = require("globals");
+const js = require("@eslint/js");
+
+module.exports = [
+	js.configs.recommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				sap: "readonly",
+			},
+			ecmaVersion: 2023,
+			sourceType: "script",
+		},
+		rules: {
+			"prefer-const": "warn",
+		},
+	},
+	{
+		ignores: ["eslint.config.js"],
+	},
+];
